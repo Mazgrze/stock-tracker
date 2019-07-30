494798329a4e152771530d3a5a5939d04c46c29d
Grzegorz Mazurek <mazurek.grz@gmail.com>
2019-07-30 12:07:11 +0200

    Updates

---
+++ precache-manifest.a2dbe5a562acd8a1ecccce31a993598c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bee9369fba25c3ca21950aeeaaface",
+    "url": "/stock-tracker/index.html"
+  },
+  {
+    "revision": "8f6d80d947f2cb5b52e0",
+    "url": "/stock-tracker/static/css/2.ceb45910.chunk.css"
+  },
+  {
+    "revision": "8f6d80d947f2cb5b52e0",
+    "url": "/stock-tracker/static/js/2.553fbc03.chunk.js"
+  },
+  {
+    "revision": "bec4fbf2c7512c8164c0",
+    "url": "/stock-tracker/static/js/main.98698438.chunk.js"
+  },
+  {
+    "revision": "8b2fb0af8cd05e69132d",
+    "url": "/stock-tracker/static/js/runtime~main.409a6373.js"
+  }
+]);
